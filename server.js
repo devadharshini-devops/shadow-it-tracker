ccc874c3ee28d613cb0693fcc39ad3a54bcd4623
devadharshini-devops <devadharshinim7412@gmail.com>
2026-04-10 20:28:19 +0530

    final fix working code

--- server.js
+++ server.js
@@ -3,13 +3,31 @@ const path = require("path");
 
 const app = express();
 
+// middleware
 app.use(express.json());
 app.use(express.static(__dirname));
 
+// open index.html
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
+// 🔥 analyze API (IMPORTANT)
+app.post("/analyze", (req, res) => {
+    const appName = req.body.appName.toLowerCase();
+
+    if (appName === "linkedin") {
+        res.json({ result: "Safe App ✅" });
+    } 
+    else if (appName === "whatsapp") {
+        res.json({ result: "Moderate Risk ⚠️" });
+    }
+    else {
+        res.json({ result: "Shadow IT Risk ❌" });
+    }
+});
+
+// start server
 app.listen(3000, () => {
     console.log("Running on http://localhost:3000");
 });
